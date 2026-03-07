59c9b942ccd20a9fa383fd601e284645a44b805e
Maria Mair <mm225mz@student.lnu.se>
2026-03-07 07:47:32 +0100

    Rename dto's to differentiate them from domains

--- backend/src/main/java/com/buildings/dto/ShorelineTypeDto.java
+++ backend/src/main/java/com/buildings/dto/ShorelineTypeDto.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ShorelineType {
+public class ShorelineTypeDto {
   private int id;
   private String name;
 }
