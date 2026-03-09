7a1f4c7c7ee38d7982d29f8a857dda0c32cee278
Maria Mair <mm225mz@student.lnu.se>
2026-03-09 08:18:40 +0100

    Add query for matching region types and region ids and replace JdbcTemplate with JdbcClient

--- backend/src/main/java/com/buildings/repository/RegionTypeRepository.java
+++ backend/src/main/java/com/buildings/repository/RegionTypeRepository.java
@@ -1,30 +1,41 @@
 package com.buildings.repository;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
-import java.util.ArrayList;
+import com.buildings.domain.RegionType;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public class RegionTypeRepository {
-  private final JdbcTemplate jdbcTemplate;
+    private final JdbcClient jdbcClient;
 
-  public RegionTypeRepository(JdbcTemplate jdbcTemplate) {
-    this.jdbcTemplate = jdbcTemplate;
+  public RegionTypeRepository(JdbcClient jdbcClient) {
+    this.jdbcClient = jdbcClient;
   }
 
   public List<Integer> findAllRegionTypeIds() {
-    String sql = "SELECT rt.id " +
-                 "FROM region_type rt";
+    return jdbcClient.sql("SELECT id FROM region_type")
+      .query((rs, _) -> rs.getInt("id"))
+      .list();
+  }
 
-    try {
-      List<Integer> regionTypeIds = jdbcTemplate.query(sql, (rs, rowNum) -> {
-        int typeId = rs.getInt("id");
-        return typeId;
-        });
-      return regionTypeIds;
-    } catch (Exception e) {
-      return new ArrayList<Integer>();
-    }
+  public Map<String, RegionType> findRegionTypeByRegionIds(List<String> regionIds) {
+    return jdbcClient.sql(
+      "SELECT r.id AS region_id, rt.id, rt.name " +
+      "FROM region_type rt " +
+      "JOIN region r ON r.type_id = rt.id " +
+      "WHERE r.id IN (:ids)")
+        .param("ids", regionIds)
+        .query((rs, _) -> Map.entry(
+            rs.getString("region_id"),
+            new RegionType(
+              rs.getInt("id"), 
+              rs.getString("name"))
+        ))
+        .list()
+        .stream()
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
   }
 }
