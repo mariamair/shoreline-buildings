c672d0db62b50ff0cd0a14b8216c5ed33bfa92cd
Maria Mair <mm225mz@student.lnu.se>
2026-03-09 22:56:56 +0100

    Add possibility to fetch building entities per region

--- backend/src/main/java/com/buildings/repository/BuildingCountRepository.java
+++ backend/src/main/java/com/buildings/repository/BuildingCountRepository.java
@@ -1,138 +1,91 @@
 package com.buildings.repository;
 
-import com.buildings.domain.AreaType;
 import com.buildings.domain.BuildingCountEntity;
-import com.buildings.domain.BuildingType;
-import com.buildings.domain.Region;
-import com.buildings.domain.RegionType;
-import com.buildings.domain.ShorelineType;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
 @Repository
-@Slf4j
 public class BuildingCountRepository {
-  private final JdbcTemplate jdbcTemplate;
+  private final JdbcClient jdbcClient;
+  private final String BASE_SQL = """
+      SELECT 
+        id, 
+        year, 
+        count 
+      FROM building_count 
+      """;
 
-    public BuildingCountRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-  // Fetch a building count entity. Use LEFT JOIN to fetch a building count entity even if it doesn't have a region or types'.
-  public Optional<BuildingCountEntity> findById(Long id) {
-    log.info("Repository: Fetching building count entitity with id " + id + "...");
-
-    String sql = "SELECT b.id, b.year, b.building_count " + 
-        ", r.id AS region_id, r.name AS region_name" +
-        ", bt.id AS building_type_id, bt.name AS building_type_name" +
-        ", st.id AS shoreline_type_id, st.name AS shoreline_type_name" +
-        ", at.id AS area_type_id, at.name AS area_type_name " + 
-        "FROM building_count b " +
-        "LEFT JOIN region r ON b.region_id = r.id " +
-        "LEFT JOIN building_type bt ON b.building_type_id = bt.id " + 
-        "LEFT JOIN shoreline_type st ON b.shoreline_type_id = st.id " + 
-        "LEFT JOIN area_type at ON b.area_type_id = at.id " + 
-        "WHERE b.id = ?";
-
-        log.info(sql);
-
-    try {
-      BuildingCountEntity buildingCount = jdbcTemplate.queryForObject(sql, (rs, rowNum) -> {
-        BuildingCountEntity b = new BuildingCountEntity(
-          rs.getLong("id"), 
-          null,
-          null,
-          null,
-          null,
-          rs.getInt("year"), 
-          null);
-
-         // Create Region if it exists in database
-          if (rs.getObject("region_id") != null) {
-            Region region = new Region(
-              rs.getString("region_id"), 
-              rs.getString("region_name"), 
-              null);
-            
-            b.setRegion(region);
-          }
- 
-          // Create BuildingType if it exists in database
-          if (rs.getObject("building_type_id") != null) {
-            BuildingType buildingType = new BuildingType(
-              rs.getInt("building_type_id"), 
-              rs.getString("building_type_name"));
-            
-            b.setBuildingType(buildingType);
-          }
-
-          // Create ShorelineType if it exists in database
-          if (rs.getObject("shoreline_type_id") != null) {
-            ShorelineType shorelineType = new ShorelineType(
-              rs.getInt("shoreline_type_id"), 
-              rs.getString("shoreline_type_name"));
-            
-            b.setShorelineType(shorelineType);
-          }
-
-          // Create AreaType if it exists in database
-          if (rs.getObject("area_type_id") != null) {
-            AreaType areaType = new AreaType(
-              rs.getInt("area_type_id"), 
-              rs.getString("area_type_name"));
-            
-            b.setAreaType(areaType);
-          } 
-
-          // Add building count if it exists in database
-          if (rs.getObject("building_count") != null) {
-            b.setBuildingCount(rs.getInt("building_count"));
-          }
-
-        return b;
-      }, id);
-      return Optional.of(buildingCount);
-
-    } catch (Exception e) {
-      return Optional.empty();
-    }
+  public BuildingCountRepository(JdbcClient jdbcClient) {
+    this.jdbcClient = jdbcClient;
   }
 
- /*  public List<BuildingCountEntity> findByType(int type_id) {
-    System.out.println("Repository: Start fetching regions by type...");
+  public Optional<BuildingCountEntity> findBuildingCountEntityById(Long id) {
+    return jdbcClient.sql(BASE_SQL + "WHERE id = :id")
+      .param("id", id)
+      .query((rs, _) -> new BuildingCountEntity(
+        rs.getLong("id"), 
+        rs.getInt("year"),
+        rs.getInt("count")
+      ))
+      .optional();
+  }
 
-    String sql = "SELECT r.id, r.name, rt.id as type_id, rt.name as type_name " +
-                 "FROM region r " +
-                 "LEFT JOIN region_type rt ON r.type_id = rt.id " +
-                 "WHERE r.type_id = ?";
+  public List<BuildingCountEntity> findBuildingCountEntitiesByAreaType(int areaTypeId, int limit, int offset) {
+    return jdbcClient.sql(BASE_SQL + "WHERE area_type_id = :areaTypeId LIMIT :limit OFFSET :offset")
+      .param("areaTypeId", areaTypeId)
+      .param("limit", limit)
+      .param("offset", offset)
+      .query(mapRowsToEntity())
+      .list();
+  }
 
-    try {
-            List<BuildingCountEntity> regions = jdbcTemplate.query(sql, (rs, rowNum) -> {
-                Region r = new Region(
-                  rs.getString("id"), 
-                  rs.getString("name"), 
-                  null);
-                
-                // Create RegionType if it exists
-                if (rs.getObject("type_id") != null) {
-                    RegionType regionType = new RegionType(
-                      rs.getInt("type_id"), 
-                      rs.getString("type_name"));
+  public List<BuildingCountEntity> findBuildingCountEntitiesByBuildingType(int buildingTypeId, int limit, int offset) {
+    return jdbcClient.sql(BASE_SQL + "WHERE building_type_id = :buildingTypeId LIMIT :limit OFFSET :offset")
+      .param("buildingTypeId", buildingTypeId)
+      .param("limit", limit)
+      .param("offset", offset)
+      .query(mapRowsToEntity())
+      .list();
+  }
 
-                    r.setRegionType(regionType);
-                }
-                
-                return r;
-            }, type_id);
-            return regions;
-        } catch (Exception e) {
-            return new ArrayList<Region>();
-        }
+  public List<BuildingCountEntity> findBuildingCountEntitiesByShorelineType(int shorelineTypeId, int limit, int offset) {
+    return jdbcClient.sql(BASE_SQL + "WHERE shoreline_type_id = :shorelineTypeId LIMIT :limit OFFSET :offset")
+      .param("shorelineTypeId", shorelineTypeId)
+      .param("limit", limit)
+      .param("offset", offset)
+      .query(mapRowsToEntity())
+      .list();
+  }
 
-  } */
+  public List<BuildingCountEntity> findBuildingCountEntitiesByRegion(String regionCode, int limit, int offset) {
+    return jdbcClient.sql(BASE_SQL + "WHERE region_code = :regionCode LIMIT :limit OFFSET :offset")
+      .param("regionCode", regionCode)
+      .param("limit", limit)
+      .param("offset", offset)
+      .query(mapRowsToEntity())
+      .list();
+  }
+
+  public List<BuildingCountEntity> findAllBuildingCountEntities(
+    int limit, 
+    int offset) {
+      System.out.println("Repository: limit " + limit + " och offset " + offset);
+    return jdbcClient.sql(BASE_SQL + "LIMIT :limit OFFSET :offset")
+      .param("limit", limit)
+      .param("offset", offset)
+      .query(mapRowsToEntity())
+      .list();
+  }
+
+  private RowMapper<BuildingCountEntity> mapRowsToEntity() {
+    return (rs, _) -> new BuildingCountEntity(
+        rs.getLong("id"), 
+        rs.getInt("year"), 
+        rs.getInt("count")
+      );
+  }
 }
