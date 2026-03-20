1ebb5c6e2b4b3dc03764ef84b5ff409ff4e769c9
Maria Mair <mm225mz@student.lnu.se>
2026-03-20 08:41:07 +0100

    Add account controller class

---
+++ auth/src/controllers/AccountController.js
@@ -0,0 +1,116 @@
+/**
+ * Defines the AccountController class.
+ *
+ * @author Maria Mair <mm225mz@student.lnu.se>
+ */
+
+import { logger } from '../config/winston.js'
+import { TokenHandler } from '../utils/TokenHandler.js'
+import { NotFoundError, UnauthorizedError } from '../utils/ErrorHandler.js'
+import { AccountModel } from '../models/AccountModel.js'
+
+/**
+ * Encapsulates a controller.
+ */
+export class AccountController {
+  #tokenhandler = new TokenHandler()
+
+  // If :id is present, load the item and provide req.doc to the route.
+  // eslint-disable-next-line max-params
+  async loadAccount (req, res, next, id) {
+    try {
+      logger.silly(`Loading account document: ${id}`)
+
+      const accountDocument = await AccountModel.findById(id)
+
+      if (!accountDocument) {
+        throw new NotFoundError('User not found')
+      }
+
+      // Provide the document to req.
+      req.doc = accountDocument
+
+      logger.silly(`Loaded acccount document: ${id}`)
+
+      // Next middleware.
+      next()
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createAccount (req, res, next) {
+    try {
+      logger.silly('Creating new account document')
+
+      // Destructure request object
+      const { username, password } = req.body
+
+      const accountDocument = await AccountModel.create({
+        username,
+        password
+      })
+
+      logger.silly(`Created new account document: ${accountDocument.id}`)
+
+      res
+        .status(201)
+        .json({ 
+          status: 'success',
+          id: accountDocument.id 
+        })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async login (req, res, next) {
+    try {
+      logger.silly('Authenticating user')
+
+      const { username, password } = req.body
+      const accountDocument = await AccountModel.authenticate(username, password)
+      const account = accountDocument.toObject()
+      logger.info(account)
+      // Create access and refresh token
+      const { accessToken, refreshToken } = await this.#tokenhandler.createTokens(account)
+
+      logger.silly(`Authenticated user: ${accountDocument.id}`)
+
+      res
+        .status(200)
+        .json({
+          status: 'success',
+          accessToken,
+          refreshToken
+        })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async refreshToken (req, res, next) {
+    try {
+      const [authenticationScheme, token] = req.headers.authorization?.split(' ') ?? []
+
+      if (authenticationScheme !== 'Bearer' || !token) {
+        throw new UnauthorizedError('Invalid or missing authorization token.')
+      }
+      logger.silly('Refreshing token')
+
+      const { accessToken, refreshToken } = await this.#tokenhandler.verifyRefreshToken(token)
+
+      logger.silly('Refreshed token')
+
+      res
+        .status(201)
+        .json({
+          status: 'success',
+          accessToken,
+          refreshToken
+        })
+    } catch (error) {
+      next(error)
+    }
+  }
+}
